09942834518a312fa1adf8826c9406cb5de6bfde
ilaria <bianchi.ilaria.studente@itispaleocapa.it>
2026-02-01 11:54:52 +0100

    funzione che genera coppie casuali di lettere

--- memory.cpp
+++ memory.cpp
@@ -3,44 +3,71 @@
 #include <cstdlib>
 using namespace std;
 
+//funzione che genera le coppie
+void genera_coppie(char carte[], int d){
+	
+	//variabile che mi serve per inserire le lettere nell'array
+	int conta_carte=0;
+	//tengo traccia delle lettere che uso
+	int lettere_usate[26]={0};
+	
+	for(int i=0; i<d; i++){
+		
+		//genera lettere casuali
+		//trasforma il numero di rand in una lettera secondo la tebella ascii
+		char caratteri_casuali=rand()%26+65;
+		//se la lettera generata è libera la metto se no la rigenero
+		if(lettere_usate[caratteri_casuali-65]==1){
+		i--;
+		continue;
+		}
+		//inserisco la lettera nella posizione dell'array
+		carte[conta_carte]=caratteri_casuali;
+		//incremento conta_carte per passare alla posizione successiva
+		conta_carte++;
+		//inserisco la lettera nella posizione successiva per avere una coppia
+		carte[conta_carte]=caratteri_casuali;
+		//incremento di nuovo così al prossimo giro inserisco una nuova coppia nelle posizioni accanto
+		conta_carte++;
+		//segno la lettera occupata
+		lettere_usate[caratteri_casuali-65]=1;	
+		//se è dispari l'ultima coppia non si fa ed è nulla (/)
+		if(d%2!=0){
+		carte[d-1]='/';
+		}	
+	}		
+}
+
+
 int main(int argc, char** argv){
 	
 	srand(time(NULL));
-	cout<<"GIOCO DEL MEMORY!!\n";
+	cout<<"GIOCO DEL MEMORY!!\n\n";
 	
 	//generare casualmente la dimensione della matrice
-	int dim_tabella=rand()%3+4;
+	int dimensione=rand()%4+3;
+	cout<<"dimensione: "<<dimensione<<'*'<<dimensione<<endl;
 	//matrice quadrata di caratteri 
 	//metto la grandezza al massimo e utilizzo solo quelle che mi servono
-	char caselle[7][7];
-	//genero dei caratteri casuali e in base al numero corrispondente al codice ascii viene generata la lettera
-	char caratteri_casuali=rand()%25+65;
+	char tabella[7][7];
+	
+	char carte[24];
+	int d=dimensione*dimensione;
 	
 	//azzero array
-	for(int i=0; i<dim_tabella; i++){
+	for(int i=0; i<dimensione; i++){
 		
-		for(int j=0; j<dim_tabella; j++){
+		for(int j=0; j<dimensione; j++){
 			
-			caselle[i][j]=0;	
+			tabella[i][j]=0;	
 		}
 	}
-	//genero caselle
-	cout<<"\ncaselle: "<<endl<<endl;
-	for(int i=0; i<dim_tabella; i++){
-		
-		for(int j=0; j<dim_tabella; j++){
-			
-			//genero lettere casuali e le inserisco nella matrice
-			caratteri_casuali=rand()%25+65;
-			caselle[i][j]=caratteri_casuali;
-			cout<<caselle[i][j]<<"    ";	
-		}
-		//va a capo ogni volta che fnisce la riga
-		if(i/dim_tabella==0){
-			cout<<endl<<endl;
-		}
-	}
+	cout<<"le coppie da indovinare sono: "<<dimensione*dimensione/2<<endl<<endl;
+	//richiamo la funzione
+	genera_coppie(carte, d);
+	for(int i=0; i<d; i++) {
+        cout<<carte[i]<<" ";
+    }
 
 	return 0;
 }
-
