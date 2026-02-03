daef258d03faa343bf2ec2b705ebdad01d3a77f9
ilaria <bianchi.ilaria.studente@itispaleocapa.it>
2026-02-03 08:58:01 +0100

    funzioni + stampa matrice

--- memory.cpp
+++ memory.cpp
@@ -4,20 +4,20 @@
 using namespace std;
 
 //funzione che genera le coppie
-void genera_coppie(char carte[], int d){
-	
+void genera_coppie(char carte[], int d) {
+
 	//variabile che mi serve per inserire le lettere nell'array
 	int conta_carte=0;
 	//tengo traccia delle lettere che uso
-	int lettere_usate[26]={0};
-	
-	for(int i=0; i<d/2; i++){
-		
+	int lettere_usate[26]= {0};
+
+	for(int i=0; i<d/2; i++) {
+
 		//genera lettere casuali
 		//trasforma il numero di rand in una lettera secondo la tebella ascii
 		char caratteri_casuali=rand()%26+65;
-		//se la lettera generata è libera la metto se no la rigenero
-		if(lettere_usate[caratteri_casuali-65]==1){
+		//se la lettera generata C( libera la metto se no la rigenero
+		if(lettere_usate[caratteri_casuali-65]==1) {
 			i--;
 			continue;
 		}
@@ -27,68 +27,102 @@ void genera_coppie(char carte[], int d){
 		conta_carte++;
 		//inserisco la lettera nella posizione successiva per avere una coppia
 		carte[conta_carte]=caratteri_casuali;
-		//incremento di nuovo così al prossimo giro inserisco una nuova coppia nelle posizioni accanto
+		//incremento di nuovo cosC, al prossimo giro inserisco una nuova coppia nelle posizioni accanto
 		conta_carte++;
 		//segno la lettera occupata
-		lettere_usate[caratteri_casuali-65]=1;	
-		//se è dispari l'ultima coppia non si fa ed è nulla (/)
-		if(d%2!=0){
-		carte[d-1]='/';
-		}	
-	}		
+		lettere_usate[caratteri_casuali-65]=1;
+		//se C( dispari l'ultima coppia non si fa ed C( nulla (/)
+		if(d%2!=0) {
+			carte[d-1]='/';
+		}
+	}
 }
 // funzione che mescola le lettere generate
-void mescola_lettere(char carte[], int d){
-	
+void mescola_lettere(char carte[], int d) {
+
 	//ciclo che si ripete tante volte quante la dimensione
-	for(int i=0; i<d; i++){
+	for(int i=0; i<d; i++) {
+	    
 		//prendo una posizione casuale dell'array compresa tra la dimensiomne
 		int mescola=rand()%d;
 		//adesso scambio la lettera nella posizione i nella posizione casuale che ho generato
 		char ordine_casuale=carte[i];
-        carte[i]=carte[mescola];
-        carte[mescola]=ordine_casuale;
+		carte[i]=carte[mescola];
+		carte[mescola]=ordine_casuale;
 	}
-	
+
 }
 
 
-int main(int argc, char** argv){
-	
+int main(int argc, char** argv) {
+
 	srand(time(NULL));
 	cout<<"GIOCO DEL MEMORY!!\n\n";
-	
+
 	//generare casualmente la dimensione della matrice
 	int dimensione=rand()%4+4;
-	//matrice quadrata di caratteri 
+	//matrice quadrata di caratteri
 	//metto la grandezza al massimo e utilizzo solo quelle che mi servono
 	char tabella[7][7];
 	//dichiaro l'array carte con il massimo possibile
 	char carte[49];
 	int d=dimensione*dimensione;
-	
+
 	//azzero array
-	for(int i=0; i<dimensione; i++){
-		
-		for(int j=0; j<dimensione; j++){
-			
-			tabella[i][j]=0;	
+	for(int i=0; i<dimensione; i++) {
+
+		for(int j=0; j<dimensione; j++) {
+
+			tabella[i][j]=0;
 		}
 	}
-	cout<<"le coppie da indovinare sono: "<<dimensione*dimensione/2<<endl<<endl;
+	cout<<"le coppie da indovinare sono: "<<dimensione*dimensione/2<<endl<<endl<<endl;
 	//richiamo le funzioni
 	genera_coppie(carte, d);
 	mescola_lettere(carte, d);
 	//stampo le carte
 	//colonna parte da 0 e incrementa ogni volta che stampo il numero, quando raggiunge un multiplo del numero (dimensione) va a capo
+	
 	int colonna=0;
-	for(int i=0; i<d; i++){
-        cout<<carte[i]<<"    ";
-        colonna++;
-        if(colonna%dimensione==0){
-        	cout<<endl<<endl;
-		}
-    }
+	
+	/*
+	//stampa matrice
+	for(int i=0; i<dimensione;i++){
+	    
+	    for(int j=0; j<dimensione; j++){
+	        
+	        tabella[i][j]=carte[colonna];
+	        cout<<tabella[i][j]<<"   ";
+	        colonna++;
+	        
+	        if(colonna%dimensione==0){
+	        cout<<endl<<endl;
+	        }
+	    }
+	}
+	*/
+	for(int i=0; i<dimensione;i++){
+	    
+	    for(int j=0; j<dimensione; j++){
+	        
+	        tabella[i][j]=carte[colonna];
+	        cout<<"*"<<"   ";
+	        colonna++;
+	        
+	        if(colonna%dimensione==0){
+	        cout<<endl<<endl;
+	        }
+	    }
+	}
+	
+	cout<<"\ninserisci le coordinate della carta che vuoi girare\n";
+	int riga_carta, colonna_carta;
+	cout<<"riga: ";
+	cin>>riga_carta;
+	cout<<"colonna: ";
+	cin>>colonna_carta;
+	
+	//cout<<"\33["<<riga_carta+5<<";"<<colonna_carta+3<<"H";
 
 	return 0;
 }
